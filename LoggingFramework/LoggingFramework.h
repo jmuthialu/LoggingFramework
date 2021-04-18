92fabec27072b7045044a82f927a3a2ac5570c0f
jmuthialu <jmuthialu@yahoo.com>
2021-04-18 18:44:01 -0400

    Initial Commit

---
+++ LoggingFramework/LoggingFramework.h
@@ -0,0 +1,18 @@
+//
+//  LoggingFramework.h
+//  LoggingFramework
+//
+//  Created by Jay Muthialu on 4/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggingFramework.
+FOUNDATION_EXPORT double LoggingFrameworkVersionNumber;
+
+//! Project version string for LoggingFramework.
+FOUNDATION_EXPORT const unsigned char LoggingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingFramework/PublicHeader.h>
+
+
