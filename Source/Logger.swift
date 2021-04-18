aff58fdfa791488c091fc7d3af1a959b0f491ba1
jmuthialu <jmuthialu@yahoo.com>
2021-04-18 19:07:45 -0400

    initial

---
+++ Source/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  LoggingFramework
+//
+//  Created by Jay Muthialu on 4/18/21.
+//
+
+import Foundation
+
+class Logger {
+    static func log() {
+        print("Jay from FW")
+    }
+}
